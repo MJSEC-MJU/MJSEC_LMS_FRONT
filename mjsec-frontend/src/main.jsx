81bd5708f21540b96a4c0f215a0674976eeb591e
Jongcoding <ialleejy@gmail.com>
2025-08-06 03:13:17 +0900

    fix: cd pipline

--- mjsec-frontend/src/main.jsx
+++ mjsec-frontend/src/main.jsx
@@ -1,12 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './styles/pro.css'; // 스타일 불러오기
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <div className="main-section">
-      <App />
-    </div>
-  </React.StrictMode>
-);
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import './styles/pro.css';
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
